9df2ac128b3fcf127b736bfaeebe2389a495961f
Carbon <carbon_128@sina.com>
2020-06-09 00:09:06 -0400

    first version for banner and link

---
+++ src/components/customlink.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "styled-components"
+
+// link hover effect
+const Label = styled.a`
+  padding: 5px;
+  position: relative;
+  color: ${props => props.linkColor || "#000000"};
+  text-decoration: none;
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: ${props => props.linkColor || "#000000"};
+    visibility: hidden;
+    transform: scaleX(0);
+    transition: all 0.3s ease-in-out 0s;
+  }
+  &:hover::before {
+    visibility: visible;
+    transform: scaleX(1);
+  }
+`
+
+const CustomLink = ({ to, displayText, linkColor }) => (
+  <Label href={to} linkColor={linkColor}>
+    {displayText}
+  </Label>
+)
+
+CustomLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  displayText: PropTypes.string.isRequired,
+  linkColor: PropTypes.string,
+}
+
+CustomLink.defaultProps = {
+  linkColor: ``,
+}
+
+export default CustomLink
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,26 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Banner from "../components/banner"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    {/* <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great.</p> */}
+    {/* put banner here */}
+    <Banner
+      title="Smartchain: "
+      subtitle="The next evolution of distributed technology"
+      description="We are building a smart, secure, and adaptable gloabal ecosystem for distributing digital information."
+      link="/"
+      linkText="What is Smartchain?"
+      linkColor="#8EB4D0"
+    />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      Divider here! above is banner part
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
