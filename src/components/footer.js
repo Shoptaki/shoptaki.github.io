8a1901df5a533e2cefacf5203bd0200bc5b8a666
administrator <administrator@administordeAir.fios-router.home>
2020-06-18 20:20:11 -0400

    commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,62 +1,217 @@
 import React from 'react';
-import "./footer.css"
 import facebookicon from "../images/facebook.svg"
 import twittericon from "../images/twitter.svg"
 import linkedinicon from "../images/linkedin.svg"
+import styled from "styled-components"
+import {theme} from "../theme/GlobalStyles.js"
+import {Link} from "gatsby"
 
+const Mainfooter = styled.div`
+    color:white;
+    align-items: center;
+    top:100%;
+    font-size:.889rem;
+    position:sticky;
+    left:0;
+    bottom:0;
+    right:0;
+    margin-top: 200%;
+    overflow:hidden;
+`
+const Title= styled.div`
+    margin-bottom:0.3em;
+    margin-top:1em;
+    color:white;
+    font-size:25px;
+    @media (max-width: ${theme.mobile}){
+        margin-bottom:0.5em;
+    }
+`
+
+const Container= styled.div`
+    width:100%;
+    padding-left:30px;
+    padding-right:20px;
+    padding-bottom:20px;
+    padding-top: 20px;
+    background-color: black;  
+ `
+const Row=styled.div`
+    display: flex;
+    flex-direction: row;
+    @media (max-width: ${theme.mobile}){
+        flex-direction: column;
+        line-height: 1.5em
+    } 
+`
+const Box=styled.div`
+    margin: 10px;
+    padding: 5px;
+    font-size: 20px;
+    flex-basis:30%;  
+    @media (max-width: ${theme.mobile}){
+        margin: 0px;
+        padding: 0px;
+        font-size: 22px;
+        flex-basis:30%; 
+        margin-top: 0.5em;
+    } 
+    
+`
+const ListUnstyled=styled.ul`
+    list-style: none;
+    margin: 0%;
+    padding: 0%;
+    font-size: 30px;
+    margin-bottom: 0;
+    @media (max-width: ${theme.mobile}){
+        font-size: 40px;
+        line-height: 0.5em;
+    }
+`
+const List =styled(Link)`
+    color: grey;
+    font-size: 18px;
+    line-height: 1.8em;
+    text-decoration:underline;
+    text-decoration-color: black;
+    &:hover{
+        text-decoration:underline;
+        text-decoration-color: grey;
+    }
+`
+
+const Icon=styled.div`
+    height: 10px;
+    display: flex;
+    flex-direction:row;
+`
+const Iconbox =styled.div`
+    margin: 10px;
+    padding: 5px;
+    flex-basis:3%;  
+    @media (max-width: ${theme.mobile}){
+        padding: 0px;
+        margin-left: 0;
+        font-size: 20px;
+        margin-top: 1em;
+        flex-basis:10%; 
+        margin-bottom: 1em;
+}
+`
+
+const Social =styled.img`
+    width:110%;
+    &:hover{
+        opacity: 0.6;
+    }
+    @media (max-width: ${theme.mobile}){
+        width:80%; 
+    }
+`
+
+const FooterBottom =styled.div`
+    color:grey;
+    margin:10px;
+    padding: 5px;
+    line-height: 0em;
+    margin-top: 60px;
+    @media (max-width: ${theme.mobile}){
+        margin: 0px;
+        padding: 0px;
+        font-size: 15px;
+        margin-top: 20%;
+    }
+`
+const FooterLinks=[
+        {
+          title: "About Us",
+          subLinks: [
+            {
+              title: "Our Mission",
+              link: "/our-mission"
+            }
+          ]
+        },
+        {
+          title: "Smartchain",
+          subLinks: [
+            {
+              title: "What is Smartchain?",
+              link: "/What-is-smartchain"
+            }
+          ]
+        },
+        {
+          title: "Our Solutions",
+          subLinks: [
+            {
+              title: "Use Cases",
+              link: "/use-cases"
+            },
+            {
+              title: "Products",
+              link: "/products"
+            }
+          ]
+        }, 
+        {
+            title: "Support",
+            subLinks: [
+              {
+                title: "Contact Us",
+                link: "/contact-us"
+              }
+            ]
+          }, 
+
+    ]
 
 function Footer () {
+    
     return (
-        <div className="main-footer">
-        <div className="container">        
-        <div className="row"> 
-            {/* flexbox 1*/}
-        <div>
-        <h3 className="footer-title">About Us</h3>
-        <ul className="list-unstyled">
-            <li ><a href="https://www.facebook.com/shoptaki/">Mission</a></li>
-            <li><a href="https://www.facebook.com/shoptaki/">What is Smartchain?</a></li>
-        </ul>
-        </div>
-            {/* flexbox 2*/}
-        <div>
-        <h3 className="footer-title">Products + Solutions</h3>
-        <ul className="list-unstyled">
-            <li><a href="https://www.facebook.com/shoptaki/">Products</a></li>
-            <li><a href="https://www.facebook.com/shoptaki/">Solutions</a></li>
-        </ul>
-        </div>
-           {/* flexbox 3*/}
-        <div>
-        <h3 className="footer-title">Support</h3>
-        <ul className="list-unstyled">
-            <li><a href="https://www.facebook.com/shoptaki/">Contact us</a></li>
-            <li><a href="https://www.facebook.com/shoptaki/">FAQ</a></li>
-        </ul>
-        </div>
-        </div>
+        <Mainfooter>
+        <Container>        
+        <Row> 
+        {
+            FooterLinks.map(column=>
+                <Box>
+                    <Title>{column.title}</Title>
+                    {
+                        column.subLinks.map(link=>
+                            <ListUnstyled>
+                                <li ><List to={link.link}>{link.title}</List></li> 
+                            </ListUnstyled>
 
-        <div className="icon">
-        <div>
-            <a className= "facebook" href="https://www.facebook.com/shoptaki/" target="blank">
-            <img className="social" src={facebookicon}/></a>  
-        </div>
-        <div>
-            <a href="https://twitter.com/shoptaki?lang=en"><img className="social" src={twittericon}/></a>
-        </div>
-        <div>
-            <a href="https://www.linkedin.com/company-beta/10899440?pathWildcard=10899440"><img className="social" src={linkedinicon}/></a>
-        </div>
-        </div>
+                        )
+                    }
+                    
+                </Box>
+             )
+        }
+     
+        </Row>
+        <Icon>
+        <Iconbox>
+            <a href="https://www.facebook.com/shoptaki/" target="blank">
+            <Social src={facebookicon}></Social></a>
+        </Iconbox>
+        <Iconbox>
+            <a href="https://twitter.com/shoptaki?lang=en"><Social className="social" src={twittericon}></Social></a>
+        </Iconbox>
+        <Iconbox>
+            <a href="https://www.linkedin.com/company-beta/10899440?pathWildcard=10899440"><Social className="social" src={linkedinicon}></Social></a>
+        </Iconbox>
+        </Icon>
 
         {/* Footer Bottom */}
-        <div className="footer-bottom">
-            <p className= "text-xs-center">
+        <FooterBottom>
+            <p>
             <h7>&copy; {(new Date().getFullYear())} Shoptaki, All Rights Reserved</h7>
             </p>
-        </div>
-        </div>
-        </div>           
+        </FooterBottom>
+        </Container>
+        </Mainfooter >           
     )
     }
 export default Footer;
--- src/components/recognize.js
+++ src/components/recognize.js
@@ -1,32 +1,115 @@
 import React from 'react';
-import "./recognize.css"
 import recog1 from "../images/recog1.svg"
 import recog2 from "../images/recog2.svg"
 import recog3 from "../images/recog3.svg"
+import styled from "styled-components"
+import {theme} from "../theme/GlobalStyles.js"
+
+const Recog = styled.div`
+    margin-top: 200%;
+    left:0;
+    right:0;
+    position: sticky;
+    overflow:hidden;
+`
+const RecogContainer= styled.div`
+    background:#F8F8F8;
+    height:220px;
+    width:100%;
+    position: sticky;
+    padding-top: 25%; 
+    background: linear-gradient( -7deg, #F8F8F8 0%, #F8F8F8 70% ,white 71%);
+    @media (max-width: ${theme.mobile}){
+        background:#F8F8F8;
+        height:480px;
+        background: linear-gradient( -7deg, #F8F8F8 0%, #F8F8F8 70% ,white 80%);
+    }
+`
+
+const Subcontainer= styled.div`
+    
+    height:120px;
+    width: 260%;
+    margin-left: -10%;
+    margin-top: -27%; 
+    margin-bottom: 1em;
+    background: linear-gradient( -3.5deg, #F8F8F8 0%, #F8F8F8 68%,#93D4EF 90%); 
+    transform: rotate(-6deg);
+    @media (max-width: ${theme.mobile}){
+    background: linear-gradient( -5deg, #F8F8F8 0%, #F8F8F8 68%,#93D4EF 90%); 
+    } 
+ `
+const RecogTitle=styled.div`
+    color: black;
+    text-align: center;
+    font-family: "Helvetica Neue";
+    font-weight:100;
+    font-size: 27px;
+    margin-bottom: 0.5em;
+    margin-top:6.5em;
+    @media (max-width: ${theme.mobile}){
+        margin-top:1.2em;
+    } 
+`
+const RecogImageBox=styled.div`
+
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+
+    @media (max-width: ${theme.mobile}){
+         display: flex;
+         flex-direction: column;
+    } 
+    
+`
+const SubRecogImagebox=styled.div`
+    margin: 10px;
+    padding: 5px;
+    padding-left: 5%;
+    padding-right: 5%;
+    @media (max-width: ${theme.mobile}){
+        margin: 0px;
+        padding: 0px;
+        line-height: 3em;
+        margin-left: 27%;
+        margin-top:10%;
+    }
+`
+const RecogImage =styled.img`
+    width: 70%;
+`
+
+const RecogImage3=styled.img`
+    width: 70%;
+    margin-bottom: -15%;
+    @media (max-width: ${theme.mobile}){
+        width: 60%;
+        margin-top: -10%;
+        margin-left: 5%;
+    }
+    
+`
 
 function Recognize () {
     return (
-       
-       <div className = "recog"> 
-            
-            <div className = "reg-container">
-                <div className = "sub-container"></div>
-                <div className="recog-title">Recognized By</div>
-                <div className ="recog-image-box">
-                <div>
-                    <img className="recog-image" src={recog1}/>
-                </div>
-                <div>
-                    <img className="recog-image" src={recog2}/>
-                </div>
-                <div>
-                    <img className="recog-image3" src={recog3}/>
-                </div>
-                </div>
-            </div>
-            
-
-        </div>
+       <Recog>  
+            <RecogContainer>
+                <Subcontainer></Subcontainer>
+                <RecogTitle>Recognized By</RecogTitle>
+                <RecogImageBox>
+                <SubRecogImagebox>
+                    <RecogImage className="recog-image" src={recog1}/>
+                </SubRecogImagebox>
+                <SubRecogImagebox>
+                    <RecogImage className="recog-image" src={recog2}/>
+                </SubRecogImagebox>
+                <SubRecogImagebox>
+                    <RecogImage3 className="recog-image3" src={recog3}/>
+                </SubRecogImagebox>
+                </RecogImageBox>
+            </RecogContainer>
+        </Recog>
     )
 }
 export default Recognize
