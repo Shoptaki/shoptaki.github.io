97abdaa87fb94d9fa9eb5c68611581a6a668d835
HanJiang-cmu <63018084+HanJiang-cmu@users.noreply.github.com>
2020-07-01 13:21:30 -0400

    TuesdayupdateHan

--- src/pages/why-smartchain.js
+++ src/pages/why-smartchain.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import WhySmartchainBanner from "../components/whySmartchainBanner"
 
 import DifferentiateCard from "../components/differentiateCard"
 
 const WhySmartchain = () => (
     <Layout>
         <SEO title="Why Smartchain?" />
-
+        <WhySmartchainBanner />
         <DifferentiateCard />
    
     </Layout>
