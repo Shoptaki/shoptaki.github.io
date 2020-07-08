6b81d2ad59354523d7664ca4ce017a4e254737db
ParkerStone <parkerwstone@gmail.com>
2020-07-08 15:59:52 -0700

    adjusted layout to match mockup

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Layout from "../components/layout"
-import styled from "styled-components";
+import Layout from "../components/layout";
 
 class App extends React.Component {
   constructor(props) {
@@ -14,28 +13,29 @@ class App extends React.Component {
     }
   }
 
-  render() {
+
+render() {
     return (
       <Layout>
-      <div className="App">
-        <label htmlFor="Contact Us" style={{fontWeight: "bold", fontSize: 25}}> Contact Us </label>
+      <div className="App" style={{textAlign: "center", alignItems: "center"}}>
+        <label htmlFor="Contact Us" style={{fontWeight: "bold", fontSize: 45, color: "blue"}}> Contact Us </label>
         <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
 
           <div className="form-group">
-            <label htmlFor="name"> Name </label>
+            <label htmlFor="name" style={{marginRight: 250}}> Name </label>
             <input type="text" className="form-control" value={this.state.name}
-                   onChange={this.onNameChange.bind(this)} style={{width: 250}}/>
+                   onChange={this.onNameChange.bind(this)} style={{width: 300, display: "block", marginLeft: "auto", marginRight: "auto", borderRadius: 10}}/>
           </div>
 
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email Address</label>
+            <label htmlFor="exampleInputEmail1" style={{marginRight: 200}}>Email Address</label>
             <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email}
-                   onChange={this.onEmailChange.bind(this)} style={{width: 250}}/>
+                   onChange={this.onEmailChange.bind(this)} style={{width: 300, display: "block", marginLeft: "auto", marginRight: "auto", borderRadius: 10}}/>
           </div>
 
           <div className="mc-field-group">
             <label htmlFor="industry" style={{width: 60}}>Industry</label>
-            <select name="industry" id="mce-INDUSTRY">
+            <select name="industry" id="mce-INDUSTRY" style={{marginRight: 45, borderRadius: 7}}>
               <option value=""></option>
               <option value="banks">Banks/Financial institutes</option>
               <option value="Government">Government</option>
@@ -46,17 +46,18 @@ class App extends React.Component {
           </div>
 
           <div className="form-group">
-            <label htmlFor="company">Company</label>
+            <label htmlFor="company" style={{marginRight: 230}}>Company</label>
             <input type="company" className="form-control" aria-describedby="companyName" value={this.state.company}
-                   onChange={this.onCompanyChange.bind(this)} style={{width: 250}}/>
+                   onChange={this.onCompanyChange.bind(this)} style={{width: 300, display: "block", marginLeft: "auto", marginRight: "auto", borderRadius: 10}}/>
           </div>
 
           <div className="form-group">
-            <label htmlFor="message">Message</label>
+            <label htmlFor="message" style={{marginRight: 240}}>Message</label>
             <textarea className="form-control" rows="10" value={this.state.message}
-                      onChange={this.onMessageChange.bind(this)} style={{width: 250}}/>
+                      onChange={this.onMessageChange.bind(this)} style={{width: 300, display: "block", marginLeft: "auto", marginRight: "auto", borderRadius: 10}}/>
           </div>
 
+          <button className="captcha" style={{marginRight: 10, fontSize: 20}}>Captcha</button>
           <button type="submit" className="btn btn-primary"> Submit</button>
         </form>
 
