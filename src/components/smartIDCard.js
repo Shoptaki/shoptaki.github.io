b367cf7dfb2de346d153299b304a2782e806d1b4
Han <administrator@administordeAir.fios-router.home>
2020-07-15 13:43:55 -0400

    aaaaaa

--- src/components/smartIDCard.js
+++ src/components/smartIDCard.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import Img from "gatsby-image"
-import { useStaticQuery, graphql } from 'gatsby'
-import { SwitchTransition, CSSTransition } from "react-transition-group"
-import { GlobalStyles, theme } from "../theme/GlobalStyles"
-import CustomLink from "./customlink"
+
+import {theme}  from "../theme/GlobalStyles"
+
 import "./styles.css"; // from React Transition Group
-import TitleLiner from "./titleliner"
+
 import productShow from "../images/ProductImage.svg";
 import greenIcon from "../images/greenicon.svg"
 import underlinerBlue from "../images/underliner-blue.svg"
@@ -16,28 +14,33 @@ const Box = styled.div`
   flex-direction: 'row';
   background-color:white;
   box-shadow: 0.2rem 0.2rem 0.5rem #999999;
-  width: 50rem;
-  margin: auto;
-  marign-top:20em;
-  margin-bottom:10em;
-
-  @media (max-width: ${theme.mobile}) {
-    width: 100%;
-    margin-left: 0rem;
-  }
-`;
+  margin-top:5em;
+  margin-bottom:20em;
+  margin-left: 14%;
+  margin-right:14%;
+  height:52em;
+  @media (max-width: ${theme.tablet}) {
+       height:78rem;
+   }
+`
 
 const Container = styled.div`
   overflow: hidden;
   display: flex;
   flex-direction: 'row';
   position: relative;
-  height:50rem;
-  @media (max-width: ${theme.mobile}) {
-    flex-direction: column-reverse;
-  }
+  height:60rem;
+  padding-bottom:3rem;
+  padding-right:1rem;
+  @media (max-width: ${theme.tablet}) {
+       height:90em;
+   }
 `
 
+const verticalBox=styled.div`
+    display: flex;
+    flex-direction: 'column';
+`
 
 const SmartTitle = styled.div`
     font-weight:bold;
@@ -47,19 +50,15 @@ const SmartTitle = styled.div`
     margin-bottom:0.5em;
 `
 
-
 const SmartLiner = styled.img`
     width:300px;
     margin-left: 1.3em;
 `
 
 
-const verticalBox=styled.div`
-    display: flex;
-    flex-direction: 'column';
-`
 const SmartDescription1=styled.div`
     margin-left:2.4em;
+    margin-right:2.4em;
     
 `
 const Boldword=styled.div`
@@ -70,36 +69,71 @@ const Boldword=styled.div`
 const SmartDescription2=styled.div`
     margin-left:2.4em;
     margin-top:1em;
+    margin-right:2.4em;
 `
+
 const HorizontalBox = styled.div`
    display: flex;
    flex-direction: 'row';
 `
+const Horizontal =styled.div`
+   display: flex;  
+   flex-direction: row;
+    @media (max-width: ${theme.tablet}) {
+       flex-direction: column-reverse;
+   }
+`
 const ProductImage = styled.img`
-   margin-top:2rem;
-   margin-left:3em;
+   margin-top:3.5rem;
+   margin-left:0.5em;
    height:30rem;
    width:20rem;
+   @media (max-width: ${theme.tablet}) {
+       margin-left:0em;
+       margin-top:2.5em;
+       width:27rem;
+   }
 `
 const BenefitsTitle = styled.div`
-   margin-top:4rem;
+   margin-top:8rem;
+   font-weight:bold;
+   margin-right:2.4em;
+   @media (max-width: ${theme.tablet}) {
+   margin-left:2.5em;
+   margin-top:2.5em;
+   }
 `
 const BenefitsDescriptions = styled.div`
-    margin-top:1rem;
+    margin-top:1em;
+    marigin-bottom:0em;
+    margin-right:2.4em;
+    @media (max-width: ${theme.tablet}) {
+       margin-left:2.5em;
+    }
+
 `
 const AdditionalDescription = styled.div`
-    margin-top:3rem;
+    margin-top:2.9rem;
+    @media (max-width: ${theme.tablet}) {
+       margin-left:2.5em;
+       margin-top:2.5em;
+    }
 `
 const GreenIcon = styled.img`
     margin-top:2.9rem;
-    width:5%;
+    width:1.2rem;
     margin-right:0.2rem;
+    @media (max-width: ${theme.tablet}) {
+       margin-left:2.5em;
+       margin-right: -2em;
+       margin-top:2.5em;
+    }
 `
 
 function SmartIDCard(props) {
 
   return (
-      <Box >
+      <Box id="smartBox">
         <Container >
           <verticalBox>
             <SmartTitle>SmartID</SmartTitle>
@@ -108,7 +142,7 @@ function SmartIDCard(props) {
 identity theft and data redundancy.</SmartDescription1>
 <SmartDescription2>SmartID will be used to verify the identity of users across an assortment of platforms. </SmartDescription2>
           
-          <HorizontalBox>
+          <Horizontal>
           <ProductImage src={productShow}></ProductImage>
           <verticalBox>
          <BenefitsTitle>Benefits</BenefitsTitle>
@@ -121,11 +155,8 @@ identity theft and data redundancy.</SmartDescription1>
         <GreenIcon src={greenIcon}></GreenIcon>
         <AdditionalDescription>More secure than a traditional SSO (single sign on)</AdditionalDescription>
         </HorizontalBox>
-        <AdditionalDescription>Content to add:</AdditionalDescription>
-
-        <BenefitsDescriptions>Identity profiling from KYC, Credit Risk, Fraud risk, etc..</BenefitsDescriptions>
         </verticalBox>
-         </HorizontalBox>
+         </Horizontal>
           </verticalBox>
           
          
--- src/pages/products.js
+++ src/pages/products.js
@@ -3,10 +3,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SmartIDCard from "../components/smartIDCard"
 import FinancialPlatformCard from "../components/FinancialPlatformCard"
+import SmallCards from "../components/smallCards"
 const Products = () => (
     <Layout>
      
         <SEO title="Why Smartchain?" />
+        <SmallCards />
     <SmartIDCard />
     <FinancialPlatformCard />
     </Layout>
