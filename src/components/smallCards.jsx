fef003ccbdac1c72d2525388497e2264214195a0
Nishi Shah <nishi@airoxa.in>
2023-06-28 12:27:51 +0530

    services content and coming soon tag added

--- src/components/smallCards.jsx
+++ src/components/smallCards.jsx
@@ -30,12 +30,12 @@ const SmallCard = styled.div`
 const SmallBox = styled.div`
   background-color: white;
   box-shadow: 0.2rem 0.2rem 0.5rem #999999;
-  margin: 5em 4em;
   max-width: 27rem;
   padding: 3.5rem;
   display: flex;
   flex-direction: column;
   height: 21.5rem;
+  position: relative;
   
   @media (max-width: ${theme.tablet}) {
     margin-bottom: 3em;
@@ -96,46 +96,157 @@ const Button = styled.button`
     transform: scale(1.1);
   }
   
-`
+`;
+
+const ComingSoon = styled.div`
+  position: absolute;
+  top: -6px;
+  left: -6px;
+  width: 140px;
+  height: 140px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const ComingSoonLabel = styled.div`
+  width: 230px;
+  height: 20px;
+  transform: translate(-50%, -50%) rotate(-45deg);
+  top: 37%;
+  left: 37%;
+  position: absolute;
+  text-align: center;
+  padding: 10px 0;
+  background: #b10000;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  border-top: 4px solid #ea8101;
+  border-bottom: 4px solid #ea8101;
+  box-shadow: 0.2rem 0.2rem 0.4rem #999999;
+`;
+const ComingSoonInner = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+const FisrtCorner = styled.div`
+  width: 0;
+  height: 0;
+  border-left: 25px solid transparent;
+  border-right: 25px solid transparent;
+  border-top: 50px solid #800000;
+  position: absolute;
+  top: 0%;
+  width: 185px;
+  left: 0%;
+`;
+const SecondCorner = styled.div`
+  width: 0;
+	height: 0;
+	border-left: 33px solid transparent;
+	border-right: 26px solid transparent;
+	border-bottom: 38px solid #800000;
+  position: absolute;
+  bottom: 0%;
+  left: 0;
+  width: 177px;
+`;
+const SmallBoxElements = styled.div`
+  position: relative;
+  margin: 5rem 4rem;
+  position: relative;
+  @media (max-width: ${theme.tablet}) {
+    margin-bottom: 3em;
+    margin-top: 0em;
+  }
+`;
+const RibbinContainer = styled.div`
+  position: absolute;
+  height: 45px;
+  width: 235px;
+  border-bottom: 4px solid #ea8101;
+  border-top: 4px solid #ea8101;
+  transform: rotate(-45deg) translate(-50%,-50%);
+  top: -7%;
+  left: 7%;
+  @media (max-width: 1200px) {
+    top: -6.9%;
+    left: 9%;
+  }
+  @media (max-width: ${theme.tablet}) {
+    top: -5.6%;
+    left: 7%;
+  }
+  @media (max-width: ${theme.mobile}) {
+    display: none;
+  }
+`;
+const RibbinInnerCon = styled.div`
+  position: relative;
+  height: 100%;
+`;
+
 
 function SmallCards( {scrollHandler} ) {
   return (
     <SmallCard>
-      <SmallBox>
-        
+      <SmallBoxElements>
+        <RibbinContainer>
+          <RibbinInnerCon>
+            <FisrtCorner>
+            </FisrtCorner>
+            <SecondCorner>
+            </SecondCorner>
+          </RibbinInnerCon>
+        </RibbinContainer>
+        <SmallBox>
           <HorizontalBox>
             <SmartTitle>SmartID <TitleLiner /> </SmartTitle>
             <SmallImage src={passportImage}></SmallImage>
           </HorizontalBox>
-
           <Description>
             SmartID is a digital passport to authenticate users quickly while
             minimizing data hacks, identity theft and data redundancy.
           </Description>
-
-            <Button onClick={() => scrollHandler("SID")}>
-              <CustomLink
-                clickHandler={e =>  e.preventDefault() }
-                displayText="Smart ID"
-                linkcolor="#2E50B9"
-                arrow
-              />  
-            </Button>
-
-      </SmallBox>
-      <SmallBox>
-        
+          <Button onClick={() => scrollHandler("SID")}>
+            <CustomLink
+              clickHandler={e =>  e.preventDefault() }
+              displayText="Smart ID"
+              linkcolor="#2E50B9"
+              arrow
+            />
+          </Button>
+          <ComingSoon>
+            <ComingSoonInner>
+              <ComingSoonLabel>
+                Coming Soon
+              </ComingSoonLabel>
+            </ComingSoonInner>
+          </ComingSoon>
+        </SmallBox>
+      </SmallBoxElements>
+      <SmallBoxElements>
+        <RibbinContainer>
+          <RibbinInnerCon>
+            <FisrtCorner>
+            </FisrtCorner>
+            <SecondCorner>
+            </SecondCorner>
+          </RibbinInnerCon>
+        </RibbinContainer>
+        <SmallBox>
           <HorizontalBox>        
             <SmartTitle>Financial Platform <TitleLiner /></SmartTitle>
             <SmallImage src={bankImage}></SmallImage>
           </HorizontalBox>
-
           <Description>
             Financial institutions will be able to utilize the combination of
             Smartchain™ and SmartID to generate better A.I. business decisions
             to optimize efficiency.
           </Description>
-      
           <Button onClick={() => scrollHandler("FPC")}>
             <CustomLink
               clickHandler={e => e.preventDefault()}
@@ -144,8 +255,15 @@ function SmallCards( {scrollHandler} ) {
               arrow
             />  
           </Button>
-
-      </SmallBox>
+          <ComingSoon>
+            <ComingSoonInner>
+              <ComingSoonLabel>
+                Coming Soon
+              </ComingSoonLabel>
+            </ComingSoonInner>
+          </ComingSoon>
+        </SmallBox>
+      </SmallBoxElements>
     </SmallCard>
   )
 }
--- src/pages/service.js
+++ src/pages/service.js
@@ -17,32 +17,32 @@ const serviceData = [
     {
         image: ai,
         title: "AI & ML",
-        discribe: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et culpa dignissimos asperiores quos officiis suscipit nihil officia at nisi libero laborum laudantium consequuntur, veniam saepe? Nesciunt dolores sint autem temporibus magnam quidem reiciendis hic expedita facilis dolor officiis dicta, "
+        discribe: "AI (Artificial Intelligence) and ML (Machine Learning) are technologies that enable machines to perform tasks that typically require human intelligence. AI refers to the broader concept of machines simulating human intelligence, while ML focuses on algorithms that allow machines to learn from data and improve performance over time. These technologies have applications in various fields, including automation, data analysis, pattern recognition, and decision-making."
     },
     {
         image: iot,
         title: "IoT",
-        discribe: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et culpa dignissimos asperiores quos officiis suscipit nihil officia at nisi libero laborum laudantium"
+        discribe: "IoT, or Internet of Things, is a network of interconnected physical devices that communicate and exchange data over the internet. It enables devices to collect and share information, leading to automation, improved efficiency, and new possibilities for monitoring and controlling various aspects of the physical world."
     },
     {
         image: webDeveloper,
         title: "Web Development",
-        discribe: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et culpa dignissimos asperiores quos officiis suscipit nihil officia at nisi libero laborum laudantium consequuntur, veniam saepe? Nesciunt dolores sint autem temporibus magnam quidem reiciendis hic expedita facilis dolor officiis dicta, "
+        discribe: "Web development is the process of creating websites and web applications using programming languages like HTML, CSS, and JavaScript. It involves designing, building, and maintaining the various components that make up a website, including the user interface, functionality, and backend infrastructure."
     },
     {
         image: mobile,
         title: "Mobile Development",
-        discribe: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et culpa dignissimos asperiores quos officiis suscipit nihil officia at nisi libero laborum laudantium"
+        discribe: "Mobile development involves creating applications for smartphones and tablets. It includes developing native or cross-platform apps using languages like Swift, Java, or JavaScript. Mobile development focuses on designing user-friendly interfaces, optimizing performance, and integrating with device features to deliver engaging experiences on mobile devices."
     },
     {
         image: ui,
         title: "UI / UX Designing",
-        discribe: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et culpa dignissimos asperiores quos officiis suscipit nihil officia at nisi libero laborum laudantium consequuntur, veniam saepe? Nesciunt dolores sint autem temporibus magnam quidem reiciendis hic expedita facilis dolor officiis dicta, "
+        discribe: "UI/UX design is about creating visually appealing and user-friendly digital experiences. It involves designing interfaces, user flows, and interactions that enhance usability and satisfaction. UI focuses on aesthetics and visual elements, while UX focuses on overall user experience, research, and usability testing."
     },
     {
         image: hireDedicatedDevelopers,
         title: "Hire Dedicated Developers",
-        discribe: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et culpa dignissimos asperiores quos officiis suscipit nihil officia at nisi libero laborum laudantium"
+        discribe: "When hiring dedicated developers, you can tap into a pool of skilled professionals who will work exclusively on your projects. These developers are committed to your goals, possess expertise in their respective fields, and provide seamless collaboration, resulting in high-quality and tailored solutions for your business."
     },
 ];
 
